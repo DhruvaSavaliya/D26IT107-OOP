06a6ae67945e0abded7a9b1a93108f343f66e503
DhruvaSavaliya <dhruvasavaliya297@gmail.com>
2026-08-16 11:41:53 +0530

    Practical 3 (Part B) commited

---
+++ Practical-3/Card.java
@@ -0,0 +1,36 @@
+import java.util.Objects;
+public class Card {
+    private String rank;
+    private String suit;
+    
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    // toString()
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+
+    // equals()
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Card other = (Card) obj;
+        return rank.equals(other.rank) && suit.equals(other.suit);
+    }
+
+    // hashCode()
+    @Override
+    public int hashCode() {
+        return Objects.hash(rank, suit);
+    }
+}
+
