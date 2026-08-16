886c23dfe38fecddcbe6d380aae60746b4ca53f5
DhruvaSavaliya <dhruvasavaliya297@gmail.com>
2026-08-16 11:47:13 +0530

    Practical 4 (Part B) commited
    Implement main method to filter chat logs based on user input.

---
+++ Practical-4/driver_4b.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class driver_4b {
+
+    public static void main(String[] args) {
+
+        String[] logs = {
+            "10:05 Dhruva, Hello there!",
+            "10:06 Bhakti, How are you ?",
+            "10:07"
+        };
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter keyword: ");
+        String keyword = input.nextLine();
+
+        System.out.println(ChatFilter.filter(logs, keyword));
+
+        input.close();
+    }
+}
+
+
+
